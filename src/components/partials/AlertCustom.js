2fea771e7649910bf8d136e3becdd38b284aea07
Tra <tradaik1806@gmail.com>
2022-07-23 11:17:07 +0700

    update alert custom UI

--- src/components/partials/AlertCustom.js
+++ src/components/partials/AlertCustom.js
@@ -5,7 +5,6 @@ import IcWarning from "../../assets/images/toast/warning.svg";
 import { TailSpin } from "react-loading-icons";
 
 const AlertCustom = ({ dataItem, closeToast, status }) => {
-  
   const { message, isLoading } = dataItem;
 
   const loadIconView = (status) => {
@@ -25,20 +24,26 @@ const AlertCustom = ({ dataItem, closeToast, status }) => {
     <div className="flex flex-row items-start justify-between">
       <div className="flex flex-row items-start">
         {loadIconView(status)}
-        <div className="flex flex-col -mt-[4px] pl-6 space-y-1">
-          <span className="font-poppins font-[600] text-base text-white">
-            {message.title}
-          </span>
-          <span className="font-poppins text-sm text-[#7694DE]">
-            {message.description}
-          </span>
+        <div className="flex flex-col pl-6 space-y-1">
+          {message.title && (
+            <span className="font-poppins font-[600] text-base text-white">
+              {message.title}
+            </span>
+          )}
+          {message.description && (
+            <span className="font-poppins text-sm text-[#7694DE]">
+              {message.description}
+            </span>
+          )}
           {message.details && (
             <a
               href={`https://explore-testnet.vechain.org/transactions/${message.details?.txid}`}
               target={"_blank"}
               rel="noopener noreferrer"
               className="font-poppins text-sm text-[#FA8C16]"
-            >{message.details?.message}</a>
+            >
+              {message.details?.message}
+            </a>
           )}
         </div>
       </div>

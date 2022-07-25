516f4030f9ccddb48296282b48378c99542991a0
Tra <tradaik1806@gmail.com>
2022-07-25 14:07:36 +0700

    update icon search modal select token

--- src/components/partials/SearchBar.js
+++ src/components/partials/SearchBar.js
@@ -1,17 +1,17 @@
 import React from "react";
-import IcSearch from "../../assets/images/ic_search.png";
+import IcSearch from "../../assets/images/ic_search.svg";
 
 const SearchBar = ({ className = "" }) => {
   return (
     <div
-      className={`hidden lg:flex flex-1 flex-row space-x-4 bg-[#334158] rounded-lg py-3 px-4 ra justify-between md:hidden ${className}`}
+      className={`hidden lg:flex flex-1 flex-row items-center space-x-4 border border-solid border-[#678BCA] rounded-lg py-3 px-4 ra justify-between md:hidden ${className}`}
     >
+      <img alt="search_icon" src={IcSearch} className="w-[18px] h-[18px] object-contain" />
       <input
-        className="flex flex-grow bg-transparent focus:outline-none placeholder-grey-3 font-poppins_light appearance-none text-base w-full"
+        className="flex flex-grow bg-transparent focus:outline-none placeholder-vbDisableText font-poppins_light appearance-none text-base w-full"
         type="text"
-        placeholder={"Search name or paste address"}
+        placeholder={"Search token name"}
       />
-      <img alt="search_icon" src={IcSearch} className="w-4.5 h-4.5 object-contain" />
     </div>
   );
 };

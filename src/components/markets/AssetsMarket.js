ac911867f00694dee72ab656eafb1f75debe2ebd
Nguyen Huu Hanh <hanh.nguyen081191@gmail.com>
2022-08-01 16:28:30 +0700

    update check ui

--- src/components/markets/AssetsMarket.js
+++ src/components/markets/AssetsMarket.js
@@ -81,7 +81,7 @@ const AssetsMarket = () => {
                             </div>
 
                             <div className="p-2 flex flex-col justify-center items-center content-center">
-                                <div className="text-lg font-semibold">{item.supplyAPY} %</div>
+                                <div className="text-base font-semibold">{item.supplyAPY} %</div>
                                 <div className="border-2 border-solid border-[#4F92A7] p-1">
                                     <div className="flex flex-row justify-start items-center space-x-2" >
                                         <span className="font-light text-sm">{item.interestSupply} %</span>
@@ -95,7 +95,7 @@ const AssetsMarket = () => {
                             </div>
 
                             <div className="p-2 flex flex-col justify-center items-center content-center">
-                                <div className="text-lg font-semibold">{item.borrowAPY} %</div>
+                                <div className="text-base font-semibold">{item.borrowAPY} %</div>
                                 <div className="border-2 border-solid border-[#4F92A7] p-1">
                                     <div className="flex flex-row justify-start items-center space-x-2" >
                                         <span className="font-light text-sm">{item.interestBorrow} %</span>

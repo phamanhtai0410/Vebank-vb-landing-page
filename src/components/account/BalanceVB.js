a2214e14bed434db6003691d4ec4aff9ca581e6a
Nguyen Huu Hanh <hanh.nguyen081191@gmail.com>
2022-08-02 10:47:21 +0700

    update navbar css

--- src/components/account/BalanceVB.js
+++ src/components/account/BalanceVB.js
@@ -20,8 +20,8 @@ const BalanceVB = () => {
 
   return (
     <div className="flex flex-row justify-start items-center space-x-2">
-      <img className="w-6 h-6" src={IcVB} alt="Coin VeBank" />
-      <span className="font-poppins font-medium text-slate-50 text-sm">
+      <img className="w-4 h-4" src={IcVB} alt="Coin VeBank" />
+      <span className="font-poppins font-medium text-slate-50 text-base">
         {balanceAccount ? formatBalanceString(balanceAccount) : `${0}`}
       </span>
     </div>

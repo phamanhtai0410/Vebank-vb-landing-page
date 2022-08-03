df6dd5d1f77730df0744d0c0f7877f44f04fc12e
Tra <tradaik1806@gmail.com>
2022-08-03 10:25:12 +0700

    hide coming soon

--- src/Wallet.js
+++ src/Wallet.js
@@ -30,15 +30,15 @@ const Wallet = () => {
 
     async function fetchWeb3Init(loadDefault) {
         await dispatch(actions.web3Connect(loadDefault));// true is account conected reload contract
-        // await dispatch(actions.getCurrentAssets())
+        await dispatch(actions.getCurrentAssets())
     }
 
     async function fetchAccountInit() {
 
         setTimeout(async () => {
             await dispatch(actions.instantiateVBContracts());
-            // await dispatch(actions.instantiateVetContracts());
-            // await dispatch(actions.instantiateVEUSDContracts());
+            await dispatch(actions.instantiateVetContracts());
+            await dispatch(actions.instantiateVEUSDContracts());
         }, 1000);
 
     }
